9e8099942f09031b793035c0e75e6044bc7bd515
Manuel.S <manumeco98@gmail.com>
2020-06-01 21:47:47 -0400

    Api rest jsonapi 1.0

---
+++ app/__init__.py
@@ -0,0 +1,455 @@
+                                                                        # APP:
+# for init app                                                                        
+from flask import Flask, request, jsonify
+
+# serializator 
+from flask_marshmallow import Marshmallow
+from marshmallow import fields, Schema, post_load
+
+# models:
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import String,Integer,Boolean,Float,ForeignKey, Column
+from sqlalchemy.orm import relationship
+from werkzeug.security import generate_password_hash, check_password_hash
+
+#api
+from flask_rest_jsonapi import ResourceDetail, ResourceList ,ResourceRelationship,Api
+from flask_rest_jsonapi.exceptions import ObjectNotFound
+from sqlalchemy.orm.exc import NoResultFound
+from marshmallow_jsonapi import fields
+from marshmallow_jsonapi.flask import Relationship,Schema
+import os
+
+import json
+
+
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"]="mysql+pymysql://msb:qwe@localhost/db_7?charset=utf8mb4"
+app.config["SECRET_KEY"]="SECRET_KEY_RIGHT_NOW"
+app.config["debug"]=True
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]=False
+
+db=SQLAlchemy(app)
+# mongo_driver = PyMongo(app)
+
+# db_users = mongo_driver.db.users
+
+ma= Marshmallow(app)
+
+
+# TABLES MODELS DEFINITION 
+
+
+class Category(db.Model):
+    # name que se debe poner, sino genera conflictos al importarlo
+    __tablename__='Category'
+    #Primary key como atributo
+    id=Column(Integer, primary_key=True)
+    name=Column(String(100), nullable=False)
+
+class Article(db.Model):
+    __tablename__="Article"
+    id=Column(Integer,nullable=False, primary_key=True)
+    name=Column(String(50),nullable=False)
+    price=Column(Float,nullable=False)
+    iva=Column(Integer,default=21)
+    description=Column(String(255))
+    image=Column(String(255))
+    stock=Column(Integer,default=0)
+    CategoryId = Column(Integer, ForeignKey('Category.id'), nullable=False)
+    #padre= object class Category
+    category=relationship("Category",backref="article",lazy=True)
+    def price_with_iva(self):
+        final.price= self.price*((self.iva/100)+1)
+        return pricefinal
+        
+roles_users= db.Table('roles_users',
+    db.Column('role_id', db.Integer, db.ForeignKey('role.id'), primary_key=True),
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True)
+)    
+
+class User(db.Model):
+    id=Column(Integer,nullable=False, primary_key=True)
+    username=Column(String(50),unique=True,nullable=False)
+    password_hash=Column(String(255),unique=True,nullable=False)
+    name=Column(String(50),nullable=False)
+    email=Column(String(50),nullable=False)
+    admin=Column(Boolean(),default=False)
+    role=relationship("Role",backref="user",secondary=roles_users, lazy="subquery")
+
+    @property
+    def password(self):
+        raise AttributeError("not allowed to read attribute password")
+    
+    
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash,password)
+    
+    # #Requisito en el modelo usuario para integrar con Flask-Login
+    # @property
+    # def is_authenticated(self):
+    #     return True 
+    # @property
+    # def is_anonymous(self):
+    #     return False
+    # @property
+    # def is_active(self):
+    #     return True
+    # #metodo requisito
+    # def get_id(self):
+    #     return self.id
+    # #extra, NO requisito para Flask login
+    # def is_admin(self):
+    #     return self.admin
+
+class Role(db.Model):
+    id=Column(Integer, nullable=False,primary_key=True)
+    name=Column(String(10), nullable=False)
+
+#SCHEMAS DEFINITIONS
+class RoleSchema(Schema):
+    class Meta:
+        type_="role"
+        self_view = "role_detail"
+        self_view_kwargs={"id":"<id>"}
+        self_view_many="role_list"
+    id=fields.Integer(as_string=True, dump_only=True)
+    name=fields.String()
+    user=Relationship(
+        related_view="user_list",
+        related_view_kwargs={"role_id":"<id>"},
+        self_view="role_users",
+        self_view_kwargs={"id":"<id>"},
+        many=True,
+        schema="UserSchema",
+        type_="user"
+    )
+    
+
+class UserSchema(Schema):
+    class Meta:
+        type_="user"
+        self_view = "user_detail"
+        self_view_kwargs={"id":"<id>"}
+        self_view_many="user_list"
+    id=fields.Integer(as_string=True, dump_only=True)
+    username=fields.String()
+    name=fields.String()
+    password=fields.String()
+    password_hash=fields.String(dump_only=True
+    )
+    email= fields.Email()
+    admin=fields.Boolean()
+    role=Relationship(
+        related_view="role_list",
+        related_view_kwargs={"user_id":"<id>"},
+        # endpoint relationship CUIDADO POR AQUI
+        self_view="user_roles" ,
+        self_view_kwargs={"id":"<id>"},
+        many=True,
+        schema="RoleSchema",
+        type_="role"
+    )
+
+class CategorySchema(Schema):
+    class Meta:
+        type_="category"
+        self_view = "category_detail"
+        self_view_kwargs={"id":"<id>"}
+        self_view_many="category_list"
+    id=fields.Integer(as_string=True, dump_only=True)
+    name=fields.String()
+    article=Relationship(
+        related_view="article_list",
+        related_view_kwargs={"category_id":"<id>"},
+        self_view="category_articles",
+        self_view_kwargs={"id":"<id>"},
+        many=True,
+        schema="ArticleSchema",
+        type_="article"
+    )
+
+class ArticleSchema(Schema):
+    class Meta:
+        type_="article" 
+        self_view="article_detail"
+        self_view_kwargs={"id":"<id>"}
+        self_viwe_many="article_list"
+    id=fields.Integer(as_string=True, dump_only=True)
+    name=fields.String()
+    price=fields.Float()
+    iva=fields.Float()
+    description=fields.String()
+    image=fields.String()
+    stock=fields.Integer()
+    CategoryId=fields.Integer(load_only=True)
+    category=Relationship(
+        related_view="category_detail",
+        related_view_kwargs={"article_id":"<id>"},
+        self_view="article_category",
+        self_view_kwargs={"id":"<id>"},
+        schema="CategorySchema",
+        type_="category"
+    )
+        
+#RESOURCES DEFINITIONS => data: diccionario para crear(POST) objeto de clase User/Role
+# viewkwargs :diccionario que contiene todos los argumentos a ser ejecutados(GET) por view
+
+#user
+class UserDetail(ResourceDetail):
+    def after_get(self,result):
+        return jsonify(result)
+
+    schema=UserSchema
+    data_layer={
+        "session":db.session,
+        "model":User,
+        "methods":{"after_get":after_get}
+    }
+
+class UserList(ResourceList):
+    def query(self,view_kwargs):
+        query_=self.session.query(User)
+        if view_kwargs.get("role_id") is not None:
+            try:
+                # verificamos que exista el role con ese role_id
+                self.session.query(Role).filter_by(id=view_kwargs.get["role_id"])
+            except NoResultFound :
+                raise ObjectNotFound({'parameter': 'id'}, "Role:{} not found".format(view_kwargs.get("role_id")))
+            else:
+                query_=query_.join(roles_users).join(Role).filter(Role.id==view_kwargs.get("role_id"))
+                # lista de usuarios relaciondos con dicho Role
+        return query_
+    # para crear un usuario en el rol especificado
+    # def before_create_object(self,data,view_kwargs):
+    #     if view_kwargs.get("role_id") is not None:
+    #         # obtenemos el objeto del role
+    #         role_=self.session.query(Role).filter_by(id=view_kwargs["role_id"]).one()
+    #         print(data)
+    #         # diccionario a deserializar en una instancia de User.
+    #         data["role_id"]=role_.id
+    def before_get(*args,**kwargs):
+        print(kwargs)
+    def after_get(self,result):
+            return jsonify(result)
+    get_schema_kwargs={"only":["name","email","password_hash"]}
+    schema=UserSchema
+    data_layer={
+    "session":db.session,
+    "model":User,
+    "methods":{
+        "query":query,
+        # "before_create_object":before_create_object,
+        "after_get":after_get
+        }
+    }
+    
+class UserRelationship(ResourceRelationship):
+
+    def after_get(self, result):
+        return jsonify(result)
+
+    schema=UserSchema
+    data_layer={
+        "session":db.session,
+        "model":User,
+        "after_get":after_get
+    }
+
+#Role
+class RoleDetail(ResourceDetail):
+    def after_get(self,result):
+        return jsonify(result)
+
+    schema=RoleSchema
+    data_layer={
+        "session":db.session,
+        "model":Role,
+        "methods":{"after_get":after_get}
+    }
+
+class RoleList(ResourceList):
+    def query(self,view_kwargs):
+        query_=self.session.query(Role)
+        if view_kwargs.get("user_id") is not None:
+            try:
+                # verificamos que exista el User con ese role_id
+                self.session.query(User).filter_by(id=view_kwargs["user_id"])
+            except NoResultFound :
+                raise ObjectNotFound({'parameter': 'id'}, "User:{} not found".format(view_kwargs.get("user_id")))
+            else:
+                query_=query_.join(roles_users).join(User).filter(User.id==view_kwargs.get("user_id"))
+        return query_
+    # para crear un usuario en el rol especificado
+    def before_create_object(self,data,**keywargs):
+        if view_kwargs.get("user_id") is not None:
+            # obtenemos el objeto del role
+            role_=self.session.query(User).filter(id=view_kwargs.get("user_id")).one()
+            data["user_id"]=user_.id
+
+    def after_get(self,result):
+            return jsonify(result)
+
+    schema=RoleSchema
+    data_layer={
+    "session":db.session,
+    "model":Role,
+    "methods":{
+        "query":query,
+        "before_create_object":before_create_object,
+        "after_get":after_get
+        }
+    }
+    
+class RoleRelationship(ResourceRelationship):
+
+    def after_get(self, result):
+        return jsonify(result)
+
+    schema=RoleSchema
+    data_layer={
+        "session":db.session,
+        "model":Role,
+        "after_get":after_get
+    }
+
+# Category
+class CategoryDetail(ResourceDetail):
+    def before_get_object(self,view_kwargs):
+        if view_kwargs.get("article_id") is not None:
+            try:
+                art=self.session.query(Article).filter_by(id=view_kwargs["article_id"]).one()
+            except NoResultFound :
+                ObjectNotFound({"parameter":"article_id"},"Article {} not found".format(view_kwargs.get("article_id")))
+            else:
+                if art.category is not None:
+                    view_kwargs["id"]=art.category.id
+                else:
+                    view_kwargs["id"]=None
+
+    def after_get(self,result):
+        return jsonify(result)
+
+    schema=CategorySchema
+    data_layer={
+        "session":db.session,
+        "model":Category,
+        "methods":{
+                "after_get":after_get,
+                "before_get_object":before_get_object,
+                }
+    }
+
+class CategoryList(ResourceList):
+
+    def after_get(self,result):
+            return jsonify(result)
+
+    schema=CategorySchema
+    data_layer={
+    "session":db.session,
+    "model":Category,
+    "methods":{
+        "after_get":after_get
+        }
+    }
+    
+class CategoryRelationship(ResourceRelationship):
+
+    def after_get(self, result):
+        return jsonify(result)
+
+    schema=CategorySchema
+    data_layer={
+        "session":db.session,
+        "model":Category,
+        "after_get":after_get
+    }
+
+# Articles
+class ArticleDetail(ResourceDetail):
+
+    def after_get(self,result):
+        return jsonify(result)
+
+    schema=ArticleSchema
+    data_layer={
+        "session":db.session,
+        "model":Article,
+        "methods":{"after_get":after_get}
+    }
+
+class ArticleList(ResourceList):
+    def query(self,view_kwargs):
+        query_=self.session.query(Article)
+        if view_kwargs.get("category_id") is not None:
+            try:
+                self.session.query(Category).filter_by(id=view_kwargs["category_id"]).one()
+            except NoResultFound:
+                raise ObjectNotFound({'parameter': 'id'}, "Category:{} not found".format(view_kwargs.get("category_id")))
+            else:
+                query_=query_.filter(Article.CategoryId == view_kwargs.get("category_id"))
+                # retornando lista de los usuarios relcionados con el rol especifiado por el id
+        return query_
+    def before_create_object(self,data, view_kwargs):
+        if view_kwargs.get("category_id") is not None:
+            category_=self.session.query(Category).filter_by(id=view_kwargs["category_id"]).one()
+            data["CategoryId"]=category_.id
+
+    def after_get(self,result):
+            return jsonify(result)
+
+    schema=ArticleSchema
+    data_layer={
+        "session":db.session,
+        "model":Article,
+        "methods":{
+        "query":query,
+        "before_create_object":before_create_object,
+        "after_get":after_get
+        }
+    }
+    
+class ArticleRelationship(ResourceRelationship):
+
+    def after_get(self, result):
+        return jsonify(result)
+
+    schema=ArticleSchema
+    data_layer={
+        "session":db.session,
+        "model":Article,
+        "after_get":after_get
+    }
+
+
+
+#ROUTES DEFINITIONS
+api=Api(app)
+# user
+api.route(UserDetail,"user_detail","/user/<int:id>")
+api.route(UserList,"user_list","/user", "/role/<int:role_id>/user")#
+api.route(UserRelationship,"user_roles","/user/<int:id>/relationship/role")
+# role
+api.route(RoleDetail,"role_detail", "/role/<int:id>")
+api.route(RoleList,"role_list", "/role" ,"/user/<int:user_id>/role")#
+api.route(RoleRelationship,"role_users","/role/<int:id>/relationship/user")
+# category
+api.route(CategoryDetail,"category_detail","/category/<int:id>", "/article/<int:article_id>/category" )#
+api.route(CategoryList, "category_list", "/category")
+api.route(CategoryRelationship, "category_articles", "/category/<int:id>/relationship/article")
+
+# articles
+api.route(ArticleDetail,"article_detail","/article/<int:id>"  )
+api.route(ArticleList, "article_list", "/article", "/category/<int:category_id>/article")#
+api.route(ArticleRelationship, "article_category", "/article/<int:id>/relationship/category")
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
